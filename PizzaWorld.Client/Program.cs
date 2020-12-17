3a9a521413d713b8a699baa1af892d0b42bde35c
taylor stoltzfus <tcstoltzfus@gmail.com>
2020-12-17 13:16:39 -0500

    first commit

---
+++ PizzaWorld.Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using PizzaWorld.Domain.Models;
+
+namespace PizzaWorld.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAllStores();
+        }
+
+        static IEnumerable<Store> GetStores()
+        {
+             return new List<Store>(){
+                 new Store(),
+                 new Store(),
+                 new Store()
+             };
+       
+        }
+
+        static void PrintAllStores()
+        {
+            foreach(var store in GetStores())
+            {
+                System.Console.WriteLine(store);
+            }
+        }
+    }
+}
